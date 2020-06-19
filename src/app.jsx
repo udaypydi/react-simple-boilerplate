4b99b9557228fd96abe61fe207044b541e78c170
udaypydi <udaypydi333@gmail.com>
2020-06-19 23:37:17 +0530

    fix(lint): eslint errors

--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,7 @@ import './app.scss';
 export default function App() {
   return (
     <div className="bg-blue-900 flex flex-col justify-center items-center p-20">
-      <p className="text-4xl text-blue-100">Welcom To React Boilerplate!</p>
+      <p className="text-4xl text-blue-100">Welcome To React Boilerplate!</p>
       <img src={logo} className="app--image" alt="logo" />
       <Button buttonText="Get Started!" />
     </div>
