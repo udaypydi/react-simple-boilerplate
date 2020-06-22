78cf23786b6c2c7fa0b054d2cbea320aac14fcad
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-22 20:53:21 +0530

    Multiple changes - Remove the /docs page, redirect to /docs/getting-started instead - Modify /docs/[id] path component to use the new topic-links and topics-modal - Remove the unused left-side-bar component

--- website/pages/docs/index.js
+++ website/pages/docs/index.js
@@ -1,38 +1,9 @@
-import Layout from '../../components/layout'
-import Head from 'next/head'
-import { getTopicWiseSortedDocsData } from '../../lib/docs'
-import Link from 'next/link'
+import Router from 'next/router'
+import { useEffect } from 'react'
 
-export default function Docs({ topicWiseDocsData }) {
-  return (
-    <Layout>
-      <Head>
-        <title>Docs</title>
-      </Head>
-      <section>
-        React Simple Boilerplate docs are catagorized in following sections:
-        <ul>
-          {Object.keys(topicWiseDocsData).map((topic, index) => (
-            <li key={index}>
-              {topic}
-              <ul>
-                {topicWiseDocsData[topic].map(({title, id}, index) => (
-                  <li key={index}><Link href='/docs/[id]' as={`/docs/${id}`}><a>{title}</a></Link></li>
-                ))}
-              </ul> 
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
-}
-
-export async function getStaticProps() {
-  const topicWiseDocsData = getTopicWiseSortedDocsData()
-  return {
-    props: {
-      topicWiseDocsData
-    }
-  }
+export default function Docs() {
+  useEffect(() => {
+    Router.push('/docs/getting-started')
+  })
+  return null
 }
