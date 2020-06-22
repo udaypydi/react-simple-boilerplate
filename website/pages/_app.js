bee049b5fac0b91902b0d1bed822fd25e443e586
Akshay Kumar <akshay.kumar@sdiot.io>
2020-06-22 20:52:04 +0530

    Import fontawesome and configure for SSR in _app

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
